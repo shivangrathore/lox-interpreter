ab9278fb6ba7307457e55dd5a94b3e99aafec5f7
Shivang <rshivang12345@gmail.com>
2025-02-28 19:55:28 +0530

    feat: add characters parsing

--- app/main.go
+++ app/main.go
@@ -10,6 +10,10 @@ const (
 	RIGHT_PAREN rune = ')'
 	LEFT_BRACE  rune = '{'
 	RIGHT_BRACE rune = '}'
+	STAR        rune = '*'
+	DOT         rune = '.'
+	COMMA       rune = ','
+	PLUS        rune = '+'
 )
 
 func main() {
@@ -43,6 +47,14 @@ func main() {
 			fmt.Println("LEFT_BRACE { null")
 		case RIGHT_BRACE:
 			fmt.Println("RIGHT_BRACE } null")
+		case STAR:
+			fmt.Println("STAR * null")
+		case DOT:
+			fmt.Println("DOT . null")
+		case COMMA:
+			fmt.Println("COMMA , null")
+		case PLUS:
+			fmt.Println("PLUS + null")
 		}
 	}
 	fmt.Println("EOF  null")
